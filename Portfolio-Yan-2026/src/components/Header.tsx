ecde860131a13f83387cd124f850eaa878d32c2c
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2025-11-27 17:41:12 -0300

    Commit inicial Portfólio
    Commit inicial do meu portfólio para 2026

---
+++ Portfolio-Yan-2026/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import 'tailwindcss'
+
+function Header() {
+  return (
+    <header className='bg-blue-500'>
+      <h1>Minha aplicação</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ Portfolio-Yan-2026/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+
+// Exemplo simples de página Home
+function Home() {
+  return (
+    <div>
+      <Header />
+      <h2>Página Home</h2>
+    </div>
+  )
+}
+
+export default Home
