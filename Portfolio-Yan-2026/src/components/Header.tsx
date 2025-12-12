9d7b5958ecf66983d1c46d448b3557c05faa8166
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2025-12-12 17:37:28 -0300

    Logo inserida

--- Portfolio-Yan-2026/src/components/Header.tsx
+++ Portfolio-Yan-2026/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import 'tailwindcss'
+import logo from '../assets/Logo_Yan_Palmer2.png'
 
 function Header() {
   return (
     <header className='fixed top-0 right-0 left-0 bg-linear-to-r from-navBackground to-navBackground text-white h-16 flex flex-row justify-around items-center border-b-2 border-gray-800 z-50'>
-      <h1 className='text-xl font-bold'>Logo</h1>
+      <img src={logo} alt="Logo Yan Palmer" className='h-full py-1' />
       <ul className='links flex flex-row justify-around items-center w-110 h-full gap-6'>
         <li className='text-white hover:text-whiteText cursor-pointer'><a href="#home">Home</a></li>
         <li className='hover:text-whiteText cursor-pointer'><a href="#sobre">Sobre</a></li>
